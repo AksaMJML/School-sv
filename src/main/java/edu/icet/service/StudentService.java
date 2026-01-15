a92890fe2ca630ae04fdd43d8a5e63966e854cf2
AksaMJML <akaks0186@gmail.com>
2026-01-15 13:26:30 +0530

    implemented method , dto and entity classes

--- src/main/java/edu/icet/service/StudentService.java
+++ src/main/java/edu/icet/service/StudentService.java
@@ -1,5 +1,7 @@
 package edu.icet.service;
 
+import edu.icet.dto.StudentDTO;
+
 import java.util.List;
 
 public interface StudentService {
@@ -7,6 +9,6 @@ public interface StudentService {
     void addStudent(StudentDTO studentDTO);
     void updateStudent(StudentDTO studentDTO);
     void deleteStudent(Integer id);
-    List<StudentDTO> serachById(Integer id);
+    StudentDTO serachById(Integer id);
     List<StudentDTO> getAll();
 }
--- src/main/java/edu/icet/service/impl/StudentServiceImpl.java
+++ src/main/java/edu/icet/service/impl/StudentServiceImpl.java
@@ -1,9 +1,36 @@
 package edu.icet.service.impl;
 
+import edu.icet.dto.StudentDTO;
 import edu.icet.service.StudentService;
+
+import java.util.List;
 
 
 public class StudentServiceImpl implements StudentService {
 
 
+    @Override
+    public void addStudent(StudentDTO studentDTO) {
+
+    }
+
+    @Override
+    public void updateStudent(StudentDTO studentDTO) {
+
+    }
+
+    @Override
+    public void deleteStudent(Integer id) {
+
+    }
+
+    @Override
+    public StudentDTO serachById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<StudentDTO> getAll() {
+        return List.of();
+    }
 }
